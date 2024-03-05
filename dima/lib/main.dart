9184edc5f3542b7178a448ab22cba74d9e4d2a0e
GiovanniStefanizzi <100594435+GiovanniStefanizzi@users.noreply.github.com>
2024-03-05 13:33:47 +0100

    Login page

--- dima/lib/main.dart
+++ dima/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/login/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,83 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(home: Login());
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  // Constructor of HelloWorld
+  // "{...}" means that the parameters inside the brackets are optional
+  // "Key?" means that the parameter key can be null
+  const HelloWorld({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500));
+  }
+}
+
+class HelloWorldPlus extends StatelessWidget {
+  final int number;
+  final Color color;
+
+  // Constructor "Initializing formals"
+  // When possible, it is recommended to use this type of constructor
+  // We made the parameter "color" optional 
+  // and set its default value to Colors.red 
+  const HelloWorldPlus(this.number, {this.color = Colors.red, Key? key})
+      : super(key: key);
+
+  // Named Constructor
+  const HelloWorldPlus.withBlue(this.number, {Key? key})
+  // This block of code is called before the constructor
+  // The call to the super class "super(key: key)" must
+  // must always be the last one of the block
+      : color = Colors.blue,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      // You can use ${} to interpolate the value of Dart expressions within strings.
+      // The curly braces can be omitted when evaluating identifiers:
+      "Hello World $number",
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: color),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// HelloWorldGenerator generates a list of "hello worlds"
+// It takes 'count' which represents the number of "hello worlds" to return
+// Each "hello world" has a different color
+class HelloWorldGenerator extends StatelessWidget {
+  final int count;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const HelloWorldGenerator(this.count, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    // Dart support generic types
+    List<Widget> childList = [];
+    for (int i = 0; i < count; i++) {
+      childList.add(
+          // Color.fromRGBO
+          // RGBO (Red Green Blue Opacity)
+          // RGB are integers and opacity is a double
+          // The primitive type double in Dart provides 64 bits of integer precision
+          // on mobile devices they provide only 53 bits of integer precision
+          // due to Javascript constraints
+          HelloWorldPlus(i,
+              color: Color.fromRGBO(
+                16 * i % 255, // red
+                32 * i % 255, // green
+                64 * i % 255, // blue
+                1.0,          // opacity
+              )));
+    }
+    return Column(children: childList);
   }
 }
---
+++ dima/lib/pages/login/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color.fromARGB(255, 190, 205, 173),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 420,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0), // Imposta il raggio di arrotondamento
+          ),
+          margin: const EdgeInsets.only(top: 150, bottom: 300, left:40, right: 40),
+          padding: const EdgeInsets.all(20),
+          child:Form( 
+            child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText:'username',
+                      hintText: 'username',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25)
+                      )
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText:'passwords',
+                          hintText: 'passwords',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text('New user? Sign up!')
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('sign in')
+                  ),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('sign in with Google')
+                  )
+                ] 
+              )
+            )
+          ),
+      )  
+    );
+  }
+}
